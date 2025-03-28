04a73a060cb4d7ffff0f226f76c03b1babb0da81
imnxtamess <davide.morisco.work@gmail.com>
2025-03-28 14:30:17 +0100

    Add Bootstrap for styling and implement post fetching in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,37 @@ import { useEffect, useState } from "react";
 const api_endpoint = "http://localhost:3000/api/v1/posts";
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
-    fetch(api_endpoint)
+    fetchData(api_endpoint);
+  }, []);
+
+  function fetchData(url) {
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setPosts(data);
+        console.log(posts);
       });
-  }, []);
+  }
+
+  return (
+    <>
+      <div className="container my-5">
+        <div className="row  row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+          {posts.map((post) => (
+            <div className="col">
+              <div className="card h-100">
+                <div className="card-img-top">
+                  <img src={post.image} alt={post.title} />
+                </div>
+                <div className="card-body">{post.title}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
